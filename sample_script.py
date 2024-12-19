fca4d61fee5dd2370b298e59923cf7cd812478e6
Shilpa A <Shilpagt1808@gmail.com>
2024-12-18 20:12:54 -0500

    homework 5

--- sample_script.py
+++ sample_script.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -11,6 +13,8 @@ driver_path = ChromeDriverManager().install()
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
+#driver.implicitly_wait(4)
+driver.wait = WebDriverWait(driver, 10)
 
 # open the url
 driver.get('https://www.google.com/')
@@ -19,15 +23,15 @@ driver.get('https://www.google.com/')
 search = driver.find_element(By.NAME, 'q')
 search.clear()
 search.send_keys('Table')
-
+driver.wait.until(EC.element_to_be_clickable((By.NAME, 'btnK'))).click()
 # wait for 4 sec
-sleep(4)
+
 
 # click search button
-driver.find_element(By.NAME, 'btnK').click()
+# driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'table'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
